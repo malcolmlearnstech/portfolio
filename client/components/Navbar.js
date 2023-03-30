4b956e69503eab7e750392e64b5b0601ff3fb1c2
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-03-30 01:15:08 -0400

    feat: create Navbar component for test display

---
+++ client/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../index.css';
+
+export default function Navbar() {
+  return (
+    <div className="navBar">
+      This is where we want to render the Navigation Bar
+    </div>
+  );
+}
