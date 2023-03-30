21b8da46b8c549f7974fba1039f3adf8d4ff05c9
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-03-29 22:08:53 -0400

    chore: remove backend routes that aren't for reading

--- server/api/tasks.js
+++ server/api/tasks.js
@@ -21,37 +21,4 @@ router.get('/:taskId', async (req, res, next) => {
   }
 });
 
-//matches POST requests to '/api/tasks/' to CREATE A TASK
-router.post('/', async (req, res, next) => {
-  try {
-    res.status(201).send(await Task.create(req.body));
-  } catch (error) {
-    next(error);
-  }
-});
-
-//matches PUT requests to 'api/tasks/:taskId' to UPDATE A SPECIFIC TASK
-router.put('/:taskId', async (req, res, next) => {
-  try {
-    const task = await Task.findByPk(req.params.taskId, {
-      include: Task,
-    });
-    await task.update();
-    res.send(task);
-  } catch (error) {
-    next(error);
-  }
-});
-
-//matches DELETE requests to 'api/tasks/:taskId' to DELETE A SPECIFIC TASK
-router.delete('/:taskId', async (req, res, next) => {
-  try {
-    const task = await Task.findByPk(req.params.taskId, {});
-    await task.destroy();
-    res.send(task);
-  } catch (error) {
-    next(error);
-  }
-});
-
 module.exports = router;
