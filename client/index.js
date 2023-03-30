c9881fdec8b2f27f38be1644ede6356a0a7d5294
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-03-30 01:12:50 -0400

    feat: add Navbar to ReactDOM rendering

--- client/index.js
+++ client/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM, { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './store/index';
+import Navbar from './components/Navbar';
 import './index.css';
 
 // make sure this react dom create refers to the same id of the div in the index.html file
 ReactDOM.createRoot(document.getElementById('app')).render(
   <Provider store={store}>
+    <Navbar />
     <div className="indexStyle">
       <p>Hello, World</p>
     </div>
