65ca06b372438f6932deef21e4742afadbee4ae8
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-03-30 01:39:16 -0400

    refactor: use of App variable for how we render site in ReactDOM render

--- client/index.js
+++ client/index.js
@@ -3,14 +3,13 @@ import ReactDOM, { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './store/index';
 import Navbar from './components/Navbar';
+import App from './App';
 import './index.css';
 
 // make sure this react dom create refers to the same id of the div in the index.html file
 ReactDOM.createRoot(document.getElementById('app')).render(
   <Provider store={store}>
     <Navbar />
-    <div className="indexStyle">
-      <p>Hello, World</p>
-    </div>
+    <App />
   </Provider>
 );
