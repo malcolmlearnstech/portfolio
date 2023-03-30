0b668316b14465a46d2d884a2988313e16564af5
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-03-29 22:08:26 -0400

    chore: remove backend routes that aren't for reading

--- server/api/users.js
+++ server/api/users.js
@@ -21,37 +21,4 @@ router.get('/:userId', async (req, res, next) => {
   }
 });
 
-//matches POST requests to '/api/users/' to CREATE A USER
-router.post('/', async (req, res, next) => {
-  try {
-    res.status(201).send(await User.create(req.body));
-  } catch (error) {
-    next(error);
-  }
-});
-
-//matches PUT requests to '/api/users/:userId' to UPDATE A SPECIFIC USER
-router.put('/:userId', async (req, res, next) => {
-  try {
-    const user = await User.findByPk(req, params.userId, {
-      include: User,
-    });
-    await user.update();
-    res.send(user);
-  } catch (error) {
-    next(error);
-  }
-});
-
-//matches DELETE requests to '/api/users/:userID' to DELETE A SPECIFIC USER
-router.delete('/:userId', async (req, res, next) => {
-  try {
-    const user = await User.findByPk(req.params.userId, {});
-    await user.destroy();
-    res.send(user);
-  } catch (error) {
-    next(error);
-  }
-});
-
 module.exports = router;
