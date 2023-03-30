238c5629cab32890fb304fc13251dce6abea7362
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-03-29 22:29:28 -0400

    fix: edit any route made for tasks to instead be for projects

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-router.use('/tasks', require('./tasks')); //all backend tasks related requests will have to use: /api/tasks/
+router.use('/projects', require('./projects')); //all backend tasks related requests will have to use: /api/projects/
 router.use('/users', require('./users')); //all backend users related requests will have to use: /api/users/
 
 //error handling for when an API that doesn't exist in entered
