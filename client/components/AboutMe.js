706140341f918ba7eb5fb33598de1058897d11d6
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-01 00:04:19 -0400

    feat: create About Me component with placeholder text

---
+++ client/components/AboutMe.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function AboutMe() {
+  return (
+    <>
+      <div className="text-3xl text-center font-bold underline pt-5">
+        <p>Hello, World</p>
+      </div>
+      <div className="text-center italic text-lg pt-4">
+        <p>Below will display the About Me section:</p>
+      </div>
+      {'\n'}
+      <div className="text-xl italic p-8">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Augue ut lectus arcu
+        bibendum. Faucibus vitae aliquet nec ullamcorper sit amet risus nullam
+        eget. Consectetur a erat nam at lectus urna duis convallis. Pretium
+        lectus quam id leo in. Nisl pretium fusce id velit ut tortor. Duis
+        convallis convallis tellus id interdum velit.
+      </div>
+    </>
+  );
+}
