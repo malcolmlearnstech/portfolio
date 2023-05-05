602ef080a9fd47c118a4e0be5b0e946dba94fabf
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-05 01:22:55 -0400

    feat: add links to respective project GitHub repositories

--- client/components/Projects.js
+++ client/components/Projects.js
@@ -7,9 +7,20 @@ export default function Projects() {
         <p>Below will display the Projects section:</p>
       </div>
       <div className="text-center text-xl italic p-8 leading-loose">
-        <div>CityDiary</div>
-        <div>Portfolio</div>
-        <div>Boilerplate</div>
+        <div>
+          CityDiary&nbsp;
+          <a href="https://github.com/HCJM/City-DiaryV2">(GitHub)</a>
+        </div>
+        <div>
+          Portfolio
+          <a href="https://github.com/malcolmlearnstech/portfolio">(GitHub)</a>
+        </div>
+        <div>
+          Boilerplate
+          <a href="https://github.com/malcolmlearnstech/second-boilerplate">
+            (GitHub)
+          </a>
+        </div>
       </div>
     </>
   );
