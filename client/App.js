3988945e27c907c0b0550b72a0676f6e6ad23326
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-05 00:46:22 -0400

    feat: import and add Skills component into App

--- client/App.js
+++ client/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import AboutMe from './components/AboutMe';
+import Skills from './components/Skills';
 
 export default function App() {
   return (
     <div>
       <AboutMe />
+      <Skills />
     </div>
   );
 }
