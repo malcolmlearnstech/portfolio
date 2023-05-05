dd1c98566cb49fbac7e3a4943c9de6f9a92139df
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-05 02:11:45 -0400

    feat: create Contact component

---
+++ client/components/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <>
+      <div className="text-center italic text-lg pt-4">
+        <p>Below will display the Contact section:</p>
+      </div>
+      <div className="text-center text-xl italic p-8 leading-loose">
+        <div>LinkedIn</div>
+        <div>GitHub</div>
+      </div>
+    </>
+  );
+}
