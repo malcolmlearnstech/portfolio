7b12f4cdc05556592e7b9a4a5504d8011063cdcd
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-05 01:18:42 -0400

    feat: create Projects component

---
+++ client/components/Projects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Projects() {
+  return (
+    <>
+      <div className="text-center italic text-lg pt-4">
+        <p>Below will display the Projects section:</p>
+      </div>
+      <div className="text-center text-xl italic p-8 leading-loose">
+        <div>CityDiary</div>
+        <div>Portfolio</div>
+        <div>Boilerplate</div>
+      </div>
+    </>
+  );
+}
