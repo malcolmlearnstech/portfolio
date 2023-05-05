48f981b720f9dbe7ad15108badcb67aa1c775851
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-05 01:19:24 -0400

    feat: import and add Projects component into App

--- client/App.js
+++ client/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import AboutMe from './components/AboutMe';
 import Skills from './components/Skills';
+import Projects from './components/Projects';
 
 export default function App() {
   return (
     <div>
       <AboutMe />
       <Skills />
+      <Projects />
     </div>
   );
 }
