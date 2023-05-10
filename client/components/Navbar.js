cac3db15d551a989d1e75ac96956313c53feda1a
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-10 15:16:24 -0400

    style: edit sizing and font of Navigation Items

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -42,9 +42,9 @@ export default function Navbar() {
                           href={item.href}
                           className={classNames(
                             item.current
-                              ? 'bg-gray-900 text-white'
-                              : 'text-gray-300 hover:bg-teal-300 hover:text-white',
-                            'rounded-md px-3 py-2 text-sm font-medium'
+                              ? 'bg-gray-900 text-white font-serif'
+                              : 'text-gray-300 hover:bg-teal-300 hover:text-white font-serif',
+                            'rounded-md px-3 py-2 text-xl font-medium'
                           )}
                           aria-current={item.current ? 'page' : undefined}
                         >
