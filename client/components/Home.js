892ee4dca43d57390072968c993a403e0e12ad9e
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-12 00:13:08 -0400

    feat: create Home component

---
+++ client/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        I'm Malcolm Mack and <br />
+        I'm a Full Stack Software Engingeer
+      </div>
+      <div></div>
+    </>
+  );
+}
