0939b19fd7a66dfb2afe40857eb8c3a6ecf96b7c
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-12 00:17:47 -0400

    style: edit line spacing

--- client/components/Skills.js
+++ client/components/Skills.js
@@ -9,7 +9,7 @@ export default function Skills() {
       <div
         align="center"
         class="[word-spacing:-20px]"
-        className="text-xl italic p-8 leading-relaxed tracking-wide leading-loose"
+        className="text-xl italic p-8 tracking-wide leading-relaxed"
       >
         <div>Node.js&nbsp;&nbsp;Javascript&nbsp;&nbsp;Git</div>
         <div>Express&nbsp;&nbsp;HTML5&nbsp;&nbsp;CSS</div>
