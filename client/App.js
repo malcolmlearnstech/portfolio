38a94e20d80687eb4a1aac4537ee2136944978b6
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-12 00:13:32 -0400

    feat: add Home component to overall App

--- client/App.js
+++ client/App.js
@@ -3,10 +3,12 @@ import AboutMe from './components/AboutMe';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import Home from './components/Home';
 
 export default function App() {
   return (
     <div>
+      <Home />
       <AboutMe />
       <Skills />
       <Projects />
