43796d383c34942e92162bf70ae54c38364b6486
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-13 18:31:37 -0400

    feat: add new dummy intro text with alignment and margins

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { ArrowSmallRightIcon } from '@heroicons/react/24/outline';
 
 export default function Home() {
   return (
     <>
-      <div className="px-96 my-28">
-        I'm Malcolm Mack and <br />
-        I'm a Full Stack Software Engingeer
+      <div name="home" className="h-screen w-full">
+        <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+          <div>
+            <h2>I am a Full Stack Software Engineer</h2>
+            <p>
+              I have a year of experience building and designing software. I
+              enjoy working on web applications using technologies like React
+              and Tailwind.
+            </p>
+            <div>
+              <button>
+                Skills
+                <span>
+                  <ArrowSmallRightIcon />
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-      <div></div>
     </>
   );
 }
