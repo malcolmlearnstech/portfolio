dd7f26bc8d6b4f3c752b8b2fd812ebf821751664
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-13 21:57:45 -0400

    edit: create two new dummy paragraphs for About Me

--- client/components/AboutMe.js
+++ client/components/AboutMe.js
@@ -2,26 +2,42 @@ import React from 'react';
 
 export default function AboutMe() {
   return (
-    <>
-      {/*<div className="text-3xl text-center font-bold underline pt-5">
-        <p>Hello, World</p>
-  </div>*/}
-      <div className="text-center italic text-lg pt-4">
-        <p>Below will display the About Me section:</p>
+    <div name="about" className="w-full h-screen">
+      <div>
+        <div className="pb-8">
+          <p>Below will display the About Me section:</p>
+        </div>
+
+        <p>
+          As is often the case with ultra, that period of pain and deep
+          exhaustion was chased by an extended flow state. Toward the end of his
+          13th lap, he hit top gear. Rocking to playlists he had curated for the
+          occasion, he sang aloud as he ran. His 91st mile was his fastest:
+          6:43. His 19th and final loop was his victory lap. “I told the guys,
+          were going to finish the way we started: strong. And I just went for
+          it.” He ran, all out, often with his eyes closed. It was up to his
+          pacers to make sure he stayed on course, and they did. When he reached
+          Engineers Gate for the final time just before midnight on Friday,
+          after running a total of 19 loops and 116 miles, he fell to the
+          ground, elated yet spent.
+        </p>
+
+        <br />
+
+        <p>
+          As is often the case with ultra, that period of pain and deep
+          exhaustion was chased by an extended flow state. Toward the end of his
+          13th lap, he hit top gear. Rocking to playlists he had curated for the
+          occasion, he sang aloud as he ran. His 91st mile was his fastest:
+          6:43. His 19th and final loop was his victory lap. “I told the guys,
+          were going to finish the way we started: strong. And I just went for
+          it.” He ran, all out, often with his eyes closed. It was up to his
+          pacers to make sure he stayed on course, and they did. When he reached
+          Engineers Gate for the final time just before midnight on Friday,
+          after running a total of 19 loops and 116 miles, he fell to the
+          ground, elated yet spent.
+        </p>
       </div>
-      {'\n'}
-      <div className="text-xl italic p-8">
-        As is often the case with ultra, that period of pain and deep exhaustion
-        was chased by an extended flow state. Toward the end of his 13th lap, he
-        hit top gear. Rocking to playlists he had curated for the occasion, he
-        sang aloud as he ran. His 91st mile was his fastest: 6:43. His 19th and
-        final loop was his victory lap. “I told the guys, were going to finish
-        the way we started: strong. And I just went for it.” He ran, all out,
-        often with his eyes closed. It was up to his pacers to make sure he
-        stayed on course, and they did. When he reached Engineers Gate for the
-        final time just before midnight on Friday, after running a total of 19
-        loops and 116 miles, he fell to the ground, elated yet spent.
-      </div>
-    </>
+    </div>
   );
 }
