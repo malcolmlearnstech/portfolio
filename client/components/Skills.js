5b8a09c844dde902c823bd4720b8cfe299a582f4
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-15 17:12:09 -0400

    chore: import remaining technology logos from static media assets

--- client/components/Skills.js
+++ client/components/Skills.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import node from '../../public/assets/nodejs2.png';
-// import javascript from '../../public/assets/javascript2.png';
-// import git from '../../public/assets/git2.png';
+import javascript from '../../public/assets/javascript2.png';
+import git from '../../public/assets/git2.png';
+import express from '../../public/assets/express2.png';
+import html from '../../public/assets/html2.png';
+import css from '../../public/assets/css2.png';
+import sequelize1 from '../../public/assets/sequelize2.png';
+import sequelize2 from '../../public/assets/sequelize3.png';
+import postgres from '../../public/assets/postgres2.png';
+import react from '../../public/assets/react2.png';
+import redux1 from '../../public/assets/redux2.png';
+import redux2 from '../../public/assets/redux3.png';
+import tailwind from '../../public/assets/tailwind2.png';
 
 export default function Skills() {
   return (
@@ -15,7 +25,7 @@ export default function Skills() {
 
           <div>
             <div>
-              <img src={git} alt="" />
+              <img src={tailwind} alt="" />
               <p>Node</p>
             </div>
           </div>
