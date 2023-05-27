2fe41918e1e4deeddab137d25b34f73dc5c20dd8
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-05-26 22:04:38 -0400

    style: edit styling of Skills title and overall skills section text, along with vertical padding for skills description

--- client/components/Skills.js
+++ client/components/Skills.js
@@ -16,11 +16,13 @@ import tailwind from '/public/assets/tailwind2.png';
 export default function Skills() {
   return (
     <>
-      <div>
-        <div>
+      <div name="skills">
+        <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
           <div>
-            <p>Skills</p>
-            <p>Technologies I've worked with</p>
+            <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
+              Skills
+            </p>
+            <p className="py-6">Technologies I've worked with</p>
           </div>
 
           <div>
