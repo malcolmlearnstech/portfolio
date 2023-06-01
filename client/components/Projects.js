6004c8c1f172ea9dcda00e3f882e55d9b0bef305
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-01 15:50:39 -0400

    style: styling for Demo & Code text in individual project panels

--- client/components/Projects.js
+++ client/components/Projects.js
@@ -13,16 +13,20 @@ export default function Projects() {
           <p className="py-6">Check out some of my work right here</p>
         </div>
 
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px">
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
           <div className="shadow-md shadow-gray-600 rounded-lg">
             <img
               src={CityDiary}
               alt=""
               className="rounded-md duration-200 hover:scale-105"
             />
-            <div>
-              <button>Demo</button>
-              <button>Code</button>
+            <div className="flex items-center justify-center">
+              <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-110">
+                Demo
+              </button>
+              <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-110">
+                Code
+              </button>
             </div>
           </div>
         </div>
