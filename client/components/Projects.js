38ffa1ffaf4a9f62f9327418abdce8ddfa353571
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-01 14:02:38 -0400

    feat: set initial rendering template

--- client/components/Projects.js
+++ client/components/Projects.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import CityDiary from '../../public/assets/city-diary-cover.png';
+import defaultCover from '../../public/assets/no-project-cover.png';
 
 export default function Projects() {
   return (
-    <>
-      <div>Projects</div>
-    </>
+    <div name="projects">
+      <div>
+        <div>
+          <p>Projects</p>
+          <p>Check out some of my work right here</p>
+        </div>
+        <div>
+          <div>
+            <img src={CityDiary} alt="" />
+            <div>
+              <button>Demo</button>
+              <button>Code</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
