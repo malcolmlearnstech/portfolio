cfb399f020905ff7404f9e150c2e5081c1474582
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-01 14:42:42 -0400

    style: make Projects title styling, add padding to section description

--- client/components/Projects.js
+++ client/components/Projects.js
@@ -6,10 +6,13 @@ export default function Projects() {
   return (
     <div name="projects" className="w-full text-black md:h-screen">
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div>
-          <p>Projects</p>
-          <p>Check out some of my work right here</p>
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Projects
+          </p>
+          <p className="py-6">Check out some of my work right here</p>
         </div>
+
         <div>
           <div>
             <img src={CityDiary} alt="" />
