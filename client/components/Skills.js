14daa5e76bdae8c85ec1471b99cf1d7a880f85ab
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-03 23:57:02 -0400

    style: edit Skill title disply font

--- client/components/Skills.js
+++ client/components/Skills.js
@@ -95,7 +95,7 @@ export default function Skills() {
       <div name="skills">
         <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-black">
           <div>
-            <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
+            <p className="text-4xl font-bold border-b-4 border-gray-600 p-2 inline">
               Skills
             </p>
             <p className="py-6">Technologies I've worked with</p>
