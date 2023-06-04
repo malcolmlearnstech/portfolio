ba1c0212e9e7f52b6b843068298812fa32e8e792
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-03 23:55:22 -0400

    style: edit hovering size on skill display

--- client/components/Skills.js
+++ client/components/Skills.js
@@ -105,7 +105,8 @@ export default function Skills() {
             {technologies.map(({ id, src, title, style }) => (
               <div
                 key={id}
-                className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
+                className={`shadow-md hover:scale-95
+                 duration-500 py-2 rounded-lg ${style}`}
               >
                 <img src={src} alt="" className="w-36 mx-auto" />
                 <p className="mt-4">{title}</p>
