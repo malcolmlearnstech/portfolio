06017eca24333c967246fb670e9d2b2c0b9ca727
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-04 23:26:47 -0400

    feat: initial Contact component structure

--- client/components/Contact.js
+++ client/components/Contact.js
@@ -3,15 +3,12 @@ import React from 'react';
 export default function Contact() {
   return (
     <>
-      <div className="text-center italic text-lg pt-4">
-        <p>Below will display the Contact section:</p>
-      </div>
-      <div className="text-center text-xl italic leading-loose">
+      <div>
         <div>
-          <a href="https://www.linkedin.com/in/malcolmrm/">LinkedIn</a>
-        </div>
-        <div>
-          <a href="https://github.com/malcolmlearnstech">GitHub</a>
+          <div>
+            <p></p>
+            <p></p>
+          </div>
         </div>
       </div>
     </>
