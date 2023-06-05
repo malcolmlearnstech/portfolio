caf9c003a1788afa423b6764926aad0d6dbdefb4
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-04 23:53:35 -0400

    style: add tailwind in-line styling to client contact submit button

--- client/components/Contact.js
+++ client/components/Contact.js
@@ -30,7 +30,9 @@ export default function Contact() {
                 className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
               ></textarea>
 
-              <button className="">Let's talk</button>
+              <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
+                Let's talk
+              </button>
             </form>
           </div>
         </div>
