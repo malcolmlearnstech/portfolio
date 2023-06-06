ba75661a678778aed6dc810fa8bf057d8ca83c37
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-05 23:47:07 -0400

    style: change input text field font colors to black for visibility

--- client/components/Contact.js
+++ client/components/Contact.js
@@ -18,19 +18,19 @@ export default function Contact() {
                 type="text"
                 name="name"
                 placeholder="Enter your name"
-                className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+                className="p-2 bg-transparent border-2 rounded-md text-black focus:outline-none"
               />
               <input
                 type="text"
                 name="name"
                 placeholder="Enter your email"
-                className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+                className="p-2 bg-transparent border-2 rounded-md text-black focus:outline-none"
               />
               <textarea
                 name="message"
                 rows="10"
                 placeholder="Enter your message"
-                className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+                className="p-2 bg-transparent border-2 rounded-md text-black focus:outline-none"
               ></textarea>
 
               <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
