d9b52dc855590d835a6d8889942c222cfcbf9fa2
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-07 01:47:47 -0400

    feat: attributes for form reference and onSubmit event

--- client/components/Contact.js
+++ client/components/Contact.js
@@ -29,7 +29,11 @@ export default function Contact() {
   return (
     <>
       <div name="contact" className="w-full h-screen text-black p-4">
-        <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
+        <div
+          ref={form}
+          onSubmit={sendEmail}
+          className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full"
+        >
           <div className="pb-8">
             <p className="text-4xl font-bold inline border-b-4 border-gray-500">
               Contact
