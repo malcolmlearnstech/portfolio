1f97d2cb8876eb1d5c6ea647d9124b01e4cff98b
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-08 16:51:44 -0400

    style: move social media link icons further right

--- client/components/Contact.js
+++ client/components/Contact.js
@@ -94,7 +94,7 @@ export default function Contact() {
               </div>
               <div className="md:w-1/2 flex flex-col justify-center items-center">
                 {/* <div>Test Text to show on right side</div> */}
-                <div className="flex justify-center items-center space-x-8 pb-40">
+                <div className="flex justify-center items-center space-x-8 pb-40 pl-48">
                   {socialContacts.map(({ id, logo, link }) => (
                     <div key={id}>{logo}</div>
                   ))}
