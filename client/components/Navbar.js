39ae69979e4370c71cbb4be4c821d8216777a214
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-09 12:33:58 -0400

    feat: add page reference values to href properties in the navigation array

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -4,11 +4,11 @@ import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
 
 const navigation = [
-  { name: 'Home', href: '#', current: true },
-  { name: 'About Me', href: '#', current: false },
-  { name: 'Skills', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Contact', href: '#', current: false },
+  { name: 'Home', href: 'home', current: true },
+  { name: 'About Me', href: 'about', current: false },
+  { name: 'Skills', href: 'skills', current: false },
+  { name: 'Projects', href: 'projects', current: false },
+  { name: 'Contact', href: 'contact', current: false },
 ];
 
 function classNames(...classes) {
