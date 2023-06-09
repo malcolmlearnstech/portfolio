ae46ae5d45435133f3f85ac26eda870e50ad5c43
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-09 12:52:56 -0400

    feat: import Link for scrolling from react-scroll

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Fragment } from 'react';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import { Link as ScrollLink } from 'react-scroll';
 
 const navigation = [
   { name: 'Home', href: 'home', current: true },
