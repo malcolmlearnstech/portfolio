98a47bb7a04321a3393174d0feec5aa4add0b626
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-08 23:36:41 -0400

    style: edit top margin for small screen spacing

--- client/components/Contact.js
+++ client/components/Contact.js
@@ -51,7 +51,10 @@ export default function Contact() {
 
   return (
     <>
-      <div name="contact" className="w-full h-screen text-black p-4">
+      <div
+        name="contact"
+        className="w-full md:h-screen text-black p-4 mt-10 md:mt-0"
+      >
         <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
           <div className="pb-8">
             <p className="text-4xl font-bold inline border-b-4 border-gray-500">
