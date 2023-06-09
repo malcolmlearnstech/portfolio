77c5ab601ea9cc6f7ddb73810b3145cc93954caa
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-08 20:17:23 -0400

    style: indicate alignment of logos in div for large and small screens

--- client/components/Contact.js
+++ client/components/Contact.js
@@ -96,7 +96,7 @@ export default function Contact() {
                   </button>
                 </form>
               </div>
-              <div className="md:w-1/2 flex flex-col justify-center items-center pt-10">
+              <div className="md:w-1/2 flex flex-col justify-center items-center pt-10 md:items-end">
                 {/* <div>Test Text to show on right side</div> */}
                 <div className="flex justify-center items-center space-x-20 pb-40">
                   {socialContacts.map(({ id, logo, link }) => (
