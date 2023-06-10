c2d4035aefc63d60e5bbe792f40809305440e11f
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-10 18:03:00 -0400

    style: center text and increase size

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <div name="footer">
-      <div className="text-center">Footer</div>
+      <div className="text-center text-xl">Footer</div>
     </div>
   );
 }
