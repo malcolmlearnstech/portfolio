b7da90d760db07ddcd015b4c07820ac0e206c302
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-10 17:10:03 -0400

    feat: import Footer component into App

--- client/App.js
+++ client/App.js
@@ -4,6 +4,7 @@ import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Home from './components/Home';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
