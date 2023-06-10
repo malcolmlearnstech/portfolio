43fdd68d2615dd499e3f46b42a5943ba17be0246
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-10 18:07:38 -0400

    refactor: change displaying Footer placeholder text and decrease text size

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <div name="footer">
-      <div className="text-center text-xl">Footer</div>
+      <div className="text-center text-md">Built and Designed by Malcolm M</div>
     </div>
   );
 }
