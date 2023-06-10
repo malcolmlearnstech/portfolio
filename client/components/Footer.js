74b888d32d5488efaab765f12acc1fb60706eeec
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-10 18:26:15 -0400

    style: edit name to display as link when hovered over

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -3,7 +3,15 @@ import React from 'react';
 export default function Footer() {
   return (
     <div name="footer">
-      <div className="text-center text-md">Built and Designed by Malcolm M</div>
+      <div className="text-center text-md">
+        Built and Designed by{' '}
+        <a
+          href="https://github.com/malcolmlearnstech"
+          className="hover:underline"
+        >
+          Malcolm M
+        </a>
+      </div>
     </div>
   );
 }
