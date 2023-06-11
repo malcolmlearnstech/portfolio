97bd0a21ea87b192ea806d52bc453685803c42a9
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-10 21:23:08 -0400

    style: parent container styles, centering, top padding

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <div name="footer">
+    <div name="footer" className="pt-10 flex flex-col items-center">
       <div className="text-center text-md">
         Built and Designed by{' '}
         <a
