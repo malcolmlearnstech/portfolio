8e8134018327049f3f2ba69e20fe886671e706d2
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-10 21:59:34 -0400

    style: child div styling to center and bump up text so not right at bottom

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <div name="footer" className="pt-10 flex flex-col items-center">
-      <div className="text-center text-md">
+      <div className="text-center text-md mb-1 self-auto">
         Built and Designed by{' '}
         <a
           href="https://github.com/malcolmlearnstech"
