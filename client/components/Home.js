376023b41e6eebf4dfc3a3a514960c9c6a43ee27
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-11 22:22:41 -0400

    delete: remove rotating right arrow

--- client/components/Home.js
+++ client/components/Home.js
@@ -19,9 +19,9 @@ export default function Home() {
             <div>
               <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-blue-600 to-orange-400 cursor-pointer">
                 View My Resume
-                <span className="group-hover:rotate-90 duration-300">
+                {/* <span className="group-hover:rotate-90 duration-300">
                   <MdOutlineKeyboardArrowRight size={20} className="ml-1" />
-                </span>
+                </span> */}
               </button>
             </div>
           </div>
