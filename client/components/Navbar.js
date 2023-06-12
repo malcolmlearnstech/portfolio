dcc2d523614ee36a0c85e9b14cb459d8d9907a0b
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-12 14:37:59 -0400

    remove red & blue background colors on divs for visibility

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -29,7 +29,7 @@ export default function Navbar() {
         {({ open }) => (
           <>
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-              <div className="relative flex h-16 bg-red-400 items-center justify-center">
+              <div className="relative flex h-16 items-center justify-center">
                 <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
                   <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                     <span className="sr-only">Open main menu</span>
@@ -40,7 +40,7 @@ export default function Navbar() {
                     )}
                   </Disclosure.Button>
                 </div>
-                <div className="absolute bg-blue-300 inset-y-0 w-4/5 flex justify-center items-center sm:static sm:inset-auto sm:m-auto sm:pr-0">
+                <div className="absolute inset-y-0 w-4/5 flex justify-center items-center sm:static sm:inset-auto sm:m-auto sm:pr-0">
                   <div className="hidden sm:mr-6 sm:block">
                     <div className="flex flex-nowrap space-x-20 leading-loose sm:m-auto">
                       {navigation.map((nav) => {
